0d91ae3aed14c37f6eab100ecda6f2e852bf1101
kyChu <hello.kychu@gmail.com>
2019-03-19 18:35:04 +0800

    add bootloader ram isr definition

--- sysconfig/boardconfig.h
+++ sysconfig/boardconfig.h
@@ -15,8 +15,12 @@
 #define SYS_HEAP_SIZE                       256
 #define SYS_STACK_SIZE                      256
 
-//#define SYS_BOOT_ORIGIN                     0x08000000
-//#define SYS_BOOT_LENGTH                     6K
+#define BOOT_ENABLED                        0
+
+#if BOOT_ENABLED
+#define SYS_BOOT_ORIGIN                     0x08000000
+#define SYS_BOOT_LENGTH                     6K
+#endif /* BOOT_ENABLED */
 #define SYS_TEXT_ORIGIN                     0x08000000
 #define SYS_TEXT_LENGTH                     32K
 #define SYS_SRAM_ORIGIN                     0x20000000
@@ -25,7 +29,10 @@
 #define SYS_MAIN_FLASH                      FLASH_TEXT
 #define SYS_MAIN_MEMORY                     SRAM
 
-#define BOOT_ENABLED                        0
+#if BOOT_ENABLED
+/* reserve 48 word starting in RAM @ 0x20000000 */
+#define SYS_VECTOR_SIZE                     192
+#endif /* BOOT_ENABLED */
 
 #define UART_RX_CACHE_SIZE                  88
 
