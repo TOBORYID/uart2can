f74d0f441de183709b0cb7f5f93156b78fc26ffb
kyChu <hello.kychu@gmail.com>
2019-03-29 11:50:49 +0800

    add system data types

---
+++ sysconfig/SysDataTypes.h
@@ -0,0 +1,13 @@
+/*
+ * SysDataTypes.h
+ *
+ *  Created on: Mar 29, 2019
+ *      Author: kychu
+ */
+
+#ifndef SYSCONFIG_SYSDATATYPES_H_
+#define SYSCONFIG_SYSDATATYPES_H_
+
+typedef void (*PortRecvByteCallback)(uint8_t Data);
+
+#endif /* SYSCONFIG_SYSDATATYPES_H_ */
